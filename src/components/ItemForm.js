c6876f67d528a2495f550ab304d59e9399a40c78
Leissan Khan <leissan.khan@gmail.com>
2022-05-22 16:36:05 -0400

    form complete

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({setItems, items}) {
+const [name, setName] = useState('')
+const [category, setCategory] = useState('Produce')
+
+function handleName (e) {
+  setName(e.target.value)
+}
+function handleCategory (e) {
+  setCategory(e.target.value)
+}
+
+function handleSubmit (e) {
+  e.preventDefault()
+  setItems([{name: name, category: category, id: uuid()}, ...items ]) //name, category - could just use it once
+
+}
+
   return (
-    <form className="NewItem">
+    <form onSubmit={handleSubmit} className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={handleName} value={name} type="text" name="name" />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select onChange={handleCategory} value={category} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
